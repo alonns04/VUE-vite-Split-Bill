819deee23aa2dbadf7eb4ef2ac2efb7515bb5fd2
alonns04 <claudiogabrielalonso@gmail.com>
2024-03-20 14:25:17 -0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    base:'https://alonns04.github.io/VUE-vite-Split-Bill'
-  ],
+  plugins: [vue()],
+    base: 'https://alonns04.github.io/VUE-vite-Split-Bill',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
